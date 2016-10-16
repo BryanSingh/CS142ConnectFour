18825747f1dcfb51925c4909051706a1cb763c2f
Bryan Singh <bryansingh14@gmail.com>
2016-10-16 17:06:57 -0400

    Added other sample users to login.

--- src/View/userLogIn.java
+++ src/View/userLogIn.java
@@ -3,6 +3,9 @@ package View;
 class userLogIn {
     // TODO Change hardcode password and username
     static boolean auth(String username, String password) {
-        return username.equals("bryan") && password.equals("password");
+        if (username.equals("bryan") || username.equals("hans") || username.equals("johncena")) {
+            if (password.equals("password")) return true;
+        }
+        return false;
     }
 }
