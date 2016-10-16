9fefd20e6e8b2d61764f8c1a57f3a6222af39147
Bryan Singh <bryansingh14@gmail.com>
2016-10-16 17:22:04 -0400

    Added PeiceType.Java to src/Model

---
+++ src/Model/PieceType.java
@@ -0,0 +1,19 @@
+package Model;
+
+public enum PieceType 
+{
+	RED ("Red"),
+	BLACK ("Black"),
+	GREEN ("Green"),
+	YELLOW ("Yellow");
+    
+    private PieceType(String type) 
+    {
+    	
+    }
+    
+    public String getType()
+    {
+    	return null;
+    }
+}
